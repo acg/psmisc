acg/psmisc

commit c4fdae52e4fe6f2453fd77ce79e113c2037ffd70

Last 22.12 release, waiting for tp
Updated the po files, waiting for translations
peekfd off-by-one patch applied

src/peekfd.c
@@ -27,6 +27,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/syscall.h>
+#include <asm/ptrace.h>
 #include <byteswap.h>
 #include <endian.h>
 #include <sys/user.h>
@@ -54,6 +55,24 @@
 	#define REG_PARAM1 orig_gpr3
 	#define REG_PARAM2 gpr[4]
 	#define REG_PARAM3 gpr[5]
+#elif defined(ARM)
+#ifndef __ARM_EABI__
+#error arm oabi not supported
+#endif
+	#define REG_ORIG_ACCUM ARM_r7
+	#define REG_ACCUM ARM_r0
+	#define REG_PARAM1 ARM_ORIG_r0
+	#define REG_PARAM2 ARM_r1
+	#define REG_PARAM3 ARM_r2
+#elif defined(MIPS)
+#ifndef MIPSEL
+#error only little endian supported
+#endif
+	#define REG_ORIG_ACCUM regs[3]
+	#define REG_ACCUM regs[2]
+	#define REG_PARAM1 regs[4]
+	#define REG_PARAM2 regs[5]
+	#define REG_PARAM3 regs[6]
 #ifndef PT_ORIG_R3
 	#define PT_ORIG_R3 34
 #endif
@@ -180,7 +199,7 @@ int main(int argc, char **argv)
       numfds = argc - optind;
       fds = malloc(sizeof(int) * numfds);
 	  for (i = 0; i < numfds; i++)
-		fds[i] = atoi(argv[optind + i]);
+		fds[i] = atoi(argv[optind + i + 1]);
     }
 
 	attach(target_pid);
@@ -208,6 +227,17 @@ int main(int argc, char **argv)
 			regs.gpr[4] = ptrace(PTRACE_PEEKUSER, pid, 4 * PT_R4, 0);
 			regs.gpr[5] = ptrace(PTRACE_PEEKUSER, pid, 4 * PT_R5, 0);
 			regs.orig_gpr3 = ptrace(PTRACE_PEEKUSER, pid, 4 * PT_ORIG_R3, 0);
+#elif defined(ARM)
+			struct pt_regs regs;
+			ptrace(PTRACE_GETREGS, pid, 0, &regs);
+#elif defined(MIPS)
+			struct pt_regs regs;
+			long pc = ptrace(PTRACE_PEEKUSER, pid, 64, 0);
+			regs.regs[2] = ptrace(PTRACE_PEEKUSER,pid,2,0);
+			regs.regs[3] = ptrace(PTRACE_PEEKTEXT, pid, pc - 8, 0) & 0xffff;
+			regs.regs[4] = ptrace(PTRACE_PEEKUSER,pid,4,0);
+			regs.regs[5] = ptrace(PTRACE_PEEKUSER,pid,5,0);
+			regs.regs[6] = ptrace(PTRACE_PEEKUSER,pid,6,0);
 #else
 			struct user_regs_struct regs;
 			ptrace(PTRACE_GETREGS, pid, 0, &regs);

configure.ac
@@ -1,6 +1,6 @@
 dnl Process this file with autoconf to produce a configure script.
 AC_PREREQ(2.61)
-AC_INIT([psmisc],[22.11])
+AC_INIT([psmisc],[22.12])
 AC_CONFIG_SRCDIR([src/comm.h])
 AC_CONFIG_HEADER([config.h])
 AC_CONFIG_AUX_DIR([config])
@@ -65,9 +65,13 @@ AC_CHECK_MEMBERS([struct user_regs_struct.orig_eax,
 		struct user_regs_struct.rax,
 		struct user_regs_struct.rdi,
 		struct user_regs_struct.rsi,
-		struct user_regs_struct.rdx], [],[], [#include <sys/user.h>])
+		struct user_regs_struct.rdx], [],[],
+    [#include <sys/types.h #include <sys/user.h>])
 AC_CHECK_MEMBERS([struct pt_regs.orig_gpr3,
 		struct pt_regs.gpr], [],[], [#include <linux/ptrace.h>])
+AC_CHECK_MEMBERS([struct pt_regs.uregs],[],[], [#include <asm/ptrace.h>])
+AC_CHECK_MEMBERS([struct pt_regs.regs],[],[], [#include <asm/ptrace.h>])
+
 AM_CONDITIONAL(WANT_PEEKFD_I386,
  test $ac_cv_member_struct_user_regs_struct_orig_eax = yes &&
    test $ac_cv_member_struct_user_regs_struct_eax = yes &&
@@ -83,6 +87,10 @@ AM_CONDITIONAL(WANT_PEEKFD_X86_64,
 AM_CONDITIONAL(WANT_PEEKFD_PPC,
    test $ac_cv_member_struct_pt_regs_orig_gpr3 = yes &&
    test $ac_cv_member_struct_pt_regs_gpr = yes )
+AM_CONDITIONAL(WANT_PEEKFD_ARM,
+   test $ac_cv_member_struct_pt_regs_uregs = yes)
+AM_CONDITIONAL(WANT_PEEKFD_MIPS,
+   test $ac_cv_member_struct_pt_regs_regs = yes)
 
 dnl Check for language stuff
 AM_GNU_GETTEXT([external])

po/bg.po
@@ -9,10 +9,11 @@ msgid ""
 msgstr ""
 "Project-Id-Version: psmisc 22.2pre1\n"
 "Report-Msgid-Bugs-To: csmall@small.dropbear.id.au\n"
-"POT-Creation-Date: 2010-03-28 18:17+1100\n"
+"POT-Creation-Date: 2010-05-29 20:15+1000\n"
 "PO-Revision-Date: 2006-03-07 11:49+0200\n"
 "Last-Translator: Anton Zinoviev <zinoviev@debian.org>\n"
 "Language-Team: Bulgarian <dict@fsa-bg.org>\n"
+"Language: bg\n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: 8bit\n"
@@ -100,7 +101,7 @@ msgstr ""
 "Copyright © 1993-2005 Werner Almesberger и Craig Small\n"
 "\n"
 
-#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:89 src/prtstat.c:68
+#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:108 src/prtstat.c:68
 #: src/pstree.c:806
 #, c-format
 msgid ""
@@ -369,24 +370,24 @@ msgstr "Максималният брой имена е %d\n"
 msgid "%s is empty (not mounted ?)\n"
 msgstr "%s е празна (не е монтирана?)\n"
 
-#: src/peekfd.c:77
+#: src/peekfd.c:96
 #, c-format
 msgid "Error attaching to pid %i\n"
 msgstr ""
 
-#: src/peekfd.c:85
+#: src/peekfd.c:104
 #, fuzzy, c-format
 msgid "peekfd (PSmisc) %s\n"
 msgstr "pstree (PSmisc) %s\n"
 
-#: src/peekfd.c:87
+#: src/peekfd.c:106
 #, c-format
 msgid ""
 "Copyright (C) 2007 Trent Waddington\n"
 "\n"
 msgstr ""
 
-#: src/peekfd.c:97
+#: src/peekfd.c:116
 #, c-format
 msgid ""
 "Usage: peekfd [-8] [-n] [-c] [-d] [-V] [-h] <pid> [<fd> ..]\n"

po/ca.po
@@ -7,10 +7,11 @@ msgid ""
 msgstr ""
 "Project-Id-Version: psmisc 21.6cvs\n"
 "Report-Msgid-Bugs-To: csmall@small.dropbear.id.au\n"
-"POT-Creation-Date: 2010-03-28 18:17+1100\n"
+"POT-Creation-Date: 2010-05-29 20:15+1000\n"
 "PO-Revision-Date: 2004-12-08 12:46+0100\n"
 "Last-Translator: Guillem Jover <guillem@debian.org>\n"
 "Language-Team: Catalan <debian-l10n-catalan@lists.debian.org>\n"
+"Language: ca\n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=iso-8859-1\n"
 "Content-Transfer-Encoding: 8bit\n"
@@ -94,7 +95,7 @@ msgstr ""
 "Copyright (C) 1993-2002 Werner Almesberger i Craig Small\n"
 "\n"
 
-#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:89 src/prtstat.c:68
+#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:108 src/prtstat.c:68
 #: src/pstree.c:806
 #, c-format
 msgid ""
@@ -357,24 +358,24 @@ msgstr "El nombre màxim de noms és %d\n"
 msgid "%s is empty (not mounted ?)\n"
 msgstr "%s és buit (no s'ha muntat?)\n"
 
-#: src/peekfd.c:77
+#: src/peekfd.c:96
 #, c-format
 msgid "Error attaching to pid %i\n"
 msgstr ""
 
-#: src/peekfd.c:85
+#: src/peekfd.c:104
 #, fuzzy, c-format
 msgid "peekfd (PSmisc) %s\n"
 msgstr "pstree (psmisc) %s\n"
 
-#: src/peekfd.c:87
+#: src/peekfd.c:106
 #, c-format
 msgid ""
 "Copyright (C) 2007 Trent Waddington\n"
 "\n"
 msgstr ""
 
-#: src/peekfd.c:97
+#: src/peekfd.c:116
 #, c-format
 msgid ""
 "Usage: peekfd [-8] [-n] [-c] [-d] [-V] [-h] <pid> [<fd> ..]\n"

po/cs.po
@@ -7,10 +7,11 @@ msgid ""
 msgstr ""
 "Project-Id-Version: psmisc 22.11-pre1\n"
 "Report-Msgid-Bugs-To: csmall@small.dropbear.id.au\n"
-"POT-Creation-Date: 2010-03-28 18:17+1100\n"
+"POT-Creation-Date: 2010-05-29 20:15+1000\n"
 "PO-Revision-Date: 2010-03-28 19:35+0200\n"
 "Last-Translator: Petr Pisar <petr.pisar@atlas.cz>\n"
 "Language-Team: Czech <translation-team-cs@lists.sourceforge.net>\n"
+"Language: cs\n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: 8-bit\n"
@@ -99,7 +100,7 @@ msgstr ""
 "Copyright © 1993–2010 Werner Almesberger a Craig Small\n"
 "\n"
 
-#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:89 src/prtstat.c:68
+#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:108 src/prtstat.c:68
 #: src/pstree.c:806
 #, c-format
 msgid ""
@@ -361,17 +362,17 @@ msgstr "Maximální počet názvů je %d\n"
 msgid "%s is empty (not mounted ?)\n"
 msgstr "%s je prázdný (není připojen?)\n"
 
-#: src/peekfd.c:77
+#: src/peekfd.c:96
 #, c-format
 msgid "Error attaching to pid %i\n"
 msgstr "Chyba při připojování se na PID %i\n"
 
-#: src/peekfd.c:85
+#: src/peekfd.c:104
 #, c-format
 msgid "peekfd (PSmisc) %s\n"
 msgstr "peekfd (PSmisc) %s\n"
 
-#: src/peekfd.c:87
+#: src/peekfd.c:106
 #, c-format
 msgid ""
 "Copyright (C) 2007 Trent Waddington\n"
@@ -380,7 +381,7 @@ msgstr ""
 "Copyright © 2007 Trent Waddington\n"
 "\n"
 
-#: src/peekfd.c:97
+#: src/peekfd.c:116
 #, c-format
 msgid ""
 "Usage: peekfd [-8] [-n] [-c] [-d] [-V] [-h] <pid> [<fd> ..]\n"

po/de.po
@@ -10,10 +10,11 @@ msgid ""
 msgstr ""
 "Project-Id-Version: psmisc 22.11-pre1\n"
 "Report-Msgid-Bugs-To: csmall@small.dropbear.id.au\n"
-"POT-Creation-Date: 2010-03-28 18:17+1100\n"
+"POT-Creation-Date: 2010-05-29 20:15+1000\n"
 "PO-Revision-Date: 2010-03-28 23:45+0100\n"
 "Last-Translator: Roland Illig <roland.illig@gmx.de>\n"
 "Language-Team: German <translation-team-de@lists.sourceforge.net>\n"
+"Language: de\n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: 8bit\n"
@@ -100,7 +101,7 @@ msgstr ""
 "Copyright (C) 1993-2010 Werner Almesberger und Craig Small\n"
 "\n"
 
-#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:89 src/prtstat.c:68
+#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:108 src/prtstat.c:68
 #: src/pstree.c:806
 #, c-format
 msgid ""
@@ -363,17 +364,17 @@ msgstr "Die maximale Anzahl von Namen ist %d\n"
 msgid "%s is empty (not mounted ?)\n"
 msgstr "%s ist leer (Dateisystem möglicherweise nicht eingebunden?)\n"
 
-#: src/peekfd.c:77
+#: src/peekfd.c:96
 #, c-format
 msgid "Error attaching to pid %i\n"
 msgstr "Fehler beim Anhängen an PID %i\n"
 
-#: src/peekfd.c:85
+#: src/peekfd.c:104
 #, c-format
 msgid "peekfd (PSmisc) %s\n"
 msgstr "peekfd (PSmisc) %s\n"
 
-#: src/peekfd.c:87
+#: src/peekfd.c:106
 #, c-format
 msgid ""
 "Copyright (C) 2007 Trent Waddington\n"
@@ -382,7 +383,7 @@ msgstr ""
 "Copyright (C) 2007 Trent Waddington\n"
 "\n"
 
-#: src/peekfd.c:97
+#: src/peekfd.c:116
 #, c-format
 msgid ""
 "Usage: peekfd [-8] [-n] [-c] [-d] [-V] [-h] <pid> [<fd> ..]\n"

po/el.po
@@ -7,10 +7,11 @@ msgid ""
 msgstr ""
 "Project-Id-Version: psmisc-22.6\n"
 "Report-Msgid-Bugs-To: csmall@small.dropbear.id.au\n"
-"POT-Creation-Date: 2010-03-28 18:17+1100\n"
+"POT-Creation-Date: 2010-05-29 20:15+1000\n"
 "PO-Revision-Date: 2008-06-15 18:31+0300\n"
 "Last-Translator: Lefteris Dimitroulakis <edimitro@tee.gr>\n"
 "Language-Team: Greek <team@lists.gnome.gr>\n"
+"Language: el\n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: 8bit\n"
@@ -96,7 +97,7 @@ msgstr ""
 "Copyright (C) 1993-2005 Werner Almesberger και Craig Small\n"
 "\n"
 
-#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:89 src/prtstat.c:68
+#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:108 src/prtstat.c:68
 #: src/pstree.c:806
 #, c-format
 msgid ""
@@ -360,17 +361,17 @@ msgstr "Μέγιστος αριθμός ονομάτων είναι %d\n"
 msgid "%s is empty (not mounted ?)\n"
 msgstr "%s είναι άδειο (μή προσαρτημένο ;)\n"
 
-#: src/peekfd.c:77
+#: src/peekfd.c:96
 #, c-format
 msgid "Error attaching to pid %i\n"
 msgstr "Σφάλμα κατά την πρόσδεση στη διεργασία με pid %i\n"
 
-#: src/peekfd.c:85
+#: src/peekfd.c:104
 #, c-format
 msgid "peekfd (PSmisc) %s\n"
 msgstr "peekfd (PSmisc) %s\n"
 
-#: src/peekfd.c:87
+#: src/peekfd.c:106
 #, c-format
 msgid ""
 "Copyright (C) 2007 Trent Waddington\n"
@@ -379,7 +380,7 @@ msgstr ""
 "Copyright (C) 2007 Trent Waddington\n"
 "\n"
 
-#: src/peekfd.c:97
+#: src/peekfd.c:116
 #, c-format
 msgid ""
 "Usage: peekfd [-8] [-n] [-c] [-d] [-V] [-h] <pid> [<fd> ..]\n"

po/eu.po
@@ -9,10 +9,11 @@ msgid ""
 msgstr ""
 "Project-Id-Version: psmisc 22.11-pre1\n"
 "Report-Msgid-Bugs-To: csmall@small.dropbear.id.au\n"
-"POT-Creation-Date: 2010-03-28 18:17+1100\n"
+"POT-Creation-Date: 2010-05-29 20:15+1000\n"
 "PO-Revision-Date: 2010-03-29 10:09+0100\n"
 "Last-Translator: Mikel Olasagasti <hey_neken@mundurat.net>\n"
 "Language-Team: Basque <translation-team-eu@lists.sourceforge.net>\n"
+"Language: eu\n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=iso-8859-1\n"
 "Content-Transfer-Encoding: 8bit\n"
@@ -98,7 +99,7 @@ msgstr ""
 "Copyright (C) 1993-2010 Werner Almesberger eta Craig Small\n"
 "\n"
 
-#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:89 src/prtstat.c:68
+#: src/fuser.c:138 src/killall.c:680 src/peekfd.c:108 src/prtstat.c:68
 #: src/pstree.c:806
 #, c-format
 msgid ""
@@ -367,17 +368,17 @@ msgstr "Izen kopuru gehiengoa %d da\n"
 msgid "%s is empty (not mounted ?)\n"
 msgstr "%s hutsik dago (muntatu gabea?)\n"
 
-#: src/peekfd.c:77
+#: src/peekfd.c:96
 #, c-format
 msgid "Error attaching to pid %i\n"
 msgstr "Errorea %i prozesura atxikitzen\n"
 
-#: src/peekfd.c:85
+#: src/peekfd.c:104
 #, c-format
 msgid "peekfd (PSmisc) %s\n"
 msgstr "peekfd (PSmisc) %s\n"
 
-#: src/peekfd.c:87
+#: src/peekfd.c:106
 #, c-format
 msgid ""
 "Copyright (C) 2007 Trent Waddington\n"
@@ -386,7 +387,7 @@ msgstr ""
 "Copyright (C) 2007 Trent Waddington\n"
 "\n"
 
-#: src/peekfd.c:97
+#: src/peekfd.c:116
 #, c-format
 msgid ""
 "Usage: peekfd [-8] [-n] [-c] [-d] [-V] [-h] <pid> [<fd> ..]\n"
@@ -511,8 +512,8 @@ msgstr ""
 "CPU denborak\n"
 "  Prozesu hau    (erabiltzailea sistema gonbidatua blkio): %6.2f %6.2f %6.2f "
 "%6.2f\n"
-"  Seme prozesuak (erabiltzailea sistema gonbidatua):       %6.2f %6.2f %"
-"6.2f\n"
+"  Seme prozesuak (erabiltzailea sistema gonbidatua):       %6.2f %6.2f "
+"%6.2f\n"
 
 #: src/prtstat.c:189
 #, c-format

commit 3e3a5039bd878f282387fff396538fcac16de9d0

Fix a regression caused by git commit c4fdae52e4fe6f which reintroduces sf#1948351.

src/peekfd.c
@@ -199,7 +199,7 @@ int main(int argc, char **argv)
       numfds = argc - optind;
       fds = malloc(sizeof(int) * numfds);
 	  for (i = 0; i < numfds; i++)
-		fds[i] = atoi(argv[optind + i + 1]);
+		fds[i] = atoi(argv[optind + i]);
     }
 
 	attach(target_pid);